ccd67a727d586ea80660cae7fc36e1aa8a6d18a7
51mj <151241834+51mj@users.noreply.github.com>
2024-11-14 08:10:02 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
 #pipinstall
 
 import googlemaps
+import tkinter as tk
+import subprocess
 
 gmaps = googlemaps.Client(key = 'AIzaSyALFItD1OlN0HdfJ1hEo3NJEyMR1SrgJto')
+
+class LocationHomepage:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Map App")
+        self.root.geometry("1000x500")
+
+        title_frame = tk.Frame(self.root, height=40)
+        title_frame.grid_propagate(False)
+        title_frame.grid(sticky="ew")
+
+                
+        title_label = tk.Label(title_frame, text="National Park App", bg="gray", font=("Noteworthy", 36))
+        title_label.pack(pady=5)
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = LocationHomepage(root)
+    root.mainloop()
