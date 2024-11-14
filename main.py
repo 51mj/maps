a6eed599aecbc10340690204d0947f8d88e9ceb2
Ian Nichols <126183308+Unchart3d@users.noreply.github.com>
2024-11-13 21:55:19 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,3 @@
 import googlemaps
 
 gmaps = googlemaps.Client(key = 'AIzaSyALFItD1OlN0HdfJ1hEo3NJEyMR1SrgJto')
-
-<iframe width="600" height="450" style="border:0" loading="lazy" allowfullscreen
-src="https://www.google.com/maps/embed/v1/view?zoom=2&center=38.7946%2C-106.5348&key=..."></iframe> 
